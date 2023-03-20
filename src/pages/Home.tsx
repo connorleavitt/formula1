b9a4d0a2db855fbc258d279f53768fb1a3f1a2fa
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-03-20 16:09:08 -0700

    Clear up comments and unnec code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
 import { FantasyMainScoreboardLeader } from "../components/FantasyScoreboards/FantasyMainScoreboardLeader";
 import { getRaceSchedule } from "../hooks/getRaceSchedule";
-import { getWeather } from "../utilities/Weather/getWeather";
-import { positionSuccess } from "../utilities/Weather/trackWeather";
 import { NextRaceDetailedWidget } from "../widgets/CurrentSeason/NextRaceDetailedWidget";
 
 export const Home: React.FC = () => {
@@ -11,27 +8,6 @@ export const Home: React.FC = () => {
     url: "https://ergast.com/api/f1/current.json",
   });
 
-  const lat = "-37.8497" as string;
-  const long = "144.968" as string;
-  const timezone = "Australia/Melbourne";
-  // const [weather, setWeather] = useState();
-
-  // useEffect(() => {
-  //   positionSuccess();
-  // }, []);
-
-  // const positionSuccess = () => {
-  //   getWeather(parseFloat(lat), parseFloat(long), timezone)
-  //     .then((res) => {
-  //       console.log(res);
-  //       setWeather(res);
-  //     })
-  //     .catch((e) => {
-  //       console.error(e);
-  //       alert("Problem getting weather data!");
-  //     });
-  // };
-  // if (!weather) return console.log("weather issues");
   if (loading) {
     return (
       <div className="ml-20 mr-20 pb-20">
@@ -49,10 +25,7 @@ export const Home: React.FC = () => {
     <div className="home m-6">
       <h1 className="text-2xl font-bold mb-4">Welcome!</h1>
       <FantasyMainScoreboardLeader />
-      <NextRaceDetailedWidget
-        raceSchedule={raceSchedule as any}
-        // trackWeather={weather as any}
-      />
+      <NextRaceDetailedWidget raceSchedule={raceSchedule as any} />
     </div>
   );
 };
