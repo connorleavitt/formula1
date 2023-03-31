b64f88fc913be236f4eafa22b95a8be8ce85275a
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-03-31 16:46:09 -0700

    Add opacity and update logic

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -89,97 +89,109 @@ export function MobileNav() {
           )}
         </button>
       </header>
-      <div className={isMobileOpen ? "w-2/3 ml-auto h-full" : "hidden"}>
-        <ul className="flex flex-col justify-between text-md m-4">
-          <li className="mb-2">
-            <Link
-              to="/"
-              className={`block rounded-xl py-3 pl-4 ${
-                activeNav === "home" ? "active" : ""
-              }`}
-            >
-              <FontAwesomeIcon icon="house" className="pr-2" />
-              Home
-            </Link>
-          </li>
-          <li className="mb-2">
-            <button
-              onClick={() => setIsCurrentOpen(!isCurrentOpen)}
-              className="relative w-full rounded-xl py-3 pl-4 text-left"
-            >
-              <FontAwesomeIcon icon="stream" className="pr-2" />
-              Current season
-              <FontAwesomeIcon
-                icon={isCurrentOpen ? "caret-up" : "caret-down"}
-                className={
-                  isCurrentOpen
-                    ? "absolute right-4 bottom-3.5"
-                    : "absolute right-4 bottom-4"
-                }
-              />
-            </button>
-            {isCurrentOpen && (
-              <ul className="pl-6">
-                <li className="my-2">
-                  <Link
-                    to="/standings"
-                    className={`block rounded-xl py-3 pl-4 ${
-                      activeNav === "standings" ? "active" : ""
-                    }`}
-                  >
-                    <FontAwesomeIcon icon="trophy" className="pr-2" />
-                    Standings
-                  </Link>
-                </li>
-                <li className="">
-                  <Link
-                    to="/schedule"
-                    className={`block rounded-xl py-3 pl-4 ${
-                      activeNav === "schedule" ? "active" : ""
-                    }`}
-                  >
-                    <FontAwesomeIcon icon="calendar" className="pr-2" />
-                    Race schedule
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </li>
-          <li className="mb-2">
-            <Link
-              to="/fantasy"
-              className={`block rounded-xl py-3 pl-4 ${
-                activeNav === "fantasy" ? "active" : ""
-              }`}
-            >
-              <FontAwesomeIcon icon="chart-column" className="pr-2" />
-              Fantasy
-            </Link>
-            {/* &#8595; */}
-          </li>
-          <li className="mb-2">
-            <Link
-              to="/historical"
-              className={`block rounded-xl py-3 pl-4 ${
-                activeNav === "historical" ? "active" : ""
-              }`}
-            >
-              <FontAwesomeIcon icon="clock-rotate-left" className="pr-2" />
-              Historical data
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link
-              to="/about"
-              className={`block rounded-xl py-3 pl-4 ${
-                activeNav === "about" ? "active" : ""
-              }`}
-            >
-              <FontAwesomeIcon icon="book" className="pr-2" />
-              About
-            </Link>
-          </li>
-        </ul>
+      <div className={isMobileOpen ? "flex h-full" : "hidden"}>
+        <button
+          onClick={() => setIsMobileOpen(!isMobileOpen)}
+          className="w-1/3 mr-auto bg-black opacity-50 cursor-default"
+        ></button>
+        <div className="w-2/3 ml-auto h-full">
+          <ul className="flex flex-col justify-between text-md m-4">
+            <li className="mb-2">
+              <Link
+                to="/"
+                onClick={() => setIsMobileOpen(!isMobileOpen)}
+                className={`block rounded-xl py-3 pl-4 ${
+                  activeNav === "home" ? "active" : ""
+                }`}
+              >
+                <FontAwesomeIcon icon="house" className="pr-2" />
+                Home
+              </Link>
+            </li>
+            <li className="mb-2">
+              <button
+                onClick={() => setIsCurrentOpen(!isCurrentOpen)}
+                className="relative w-full rounded-xl py-3 pl-4 text-left"
+              >
+                <FontAwesomeIcon icon="stream" className="pr-2" />
+                Current season
+                <FontAwesomeIcon
+                  icon={isCurrentOpen ? "caret-up" : "caret-down"}
+                  className={
+                    isCurrentOpen
+                      ? "absolute right-4 bottom-3.5"
+                      : "absolute right-4 bottom-4"
+                  }
+                />
+              </button>
+              {isCurrentOpen && (
+                <ul className="pl-6">
+                  <li className="my-2">
+                    <Link
+                      to="/standings"
+                      onClick={() => setIsMobileOpen(!isMobileOpen)}
+                      className={`block rounded-xl py-3 pl-4 ${
+                        activeNav === "standings" ? "active" : ""
+                      }`}
+                    >
+                      <FontAwesomeIcon icon="trophy" className="pr-2" />
+                      Standings
+                    </Link>
+                  </li>
+                  <li className="">
+                    <Link
+                      to="/schedule"
+                      onClick={() => setIsMobileOpen(!isMobileOpen)}
+                      className={`block rounded-xl py-3 pl-4 ${
+                        activeNav === "schedule" ? "active" : ""
+                      }`}
+                    >
+                      <FontAwesomeIcon icon="calendar" className="pr-2" />
+                      Race schedule
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li className="mb-2">
+              <Link
+                to="/fantasy"
+                onClick={() => setIsMobileOpen(!isMobileOpen)}
+                className={`block rounded-xl py-3 pl-4 ${
+                  activeNav === "fantasy" ? "active" : ""
+                }`}
+              >
+                <FontAwesomeIcon icon="chart-column" className="pr-2" />
+                Fantasy
+              </Link>
+              {/* &#8595; */}
+            </li>
+            <li className="mb-2">
+              <Link
+                to="/historical"
+                onClick={() => setIsMobileOpen(!isMobileOpen)}
+                className={`block rounded-xl py-3 pl-4 ${
+                  activeNav === "historical" ? "active" : ""
+                }`}
+              >
+                <FontAwesomeIcon icon="clock-rotate-left" className="pr-2" />
+                Historical data
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link
+                to="/about"
+                onClick={() => setIsMobileOpen(!isMobileOpen)}
+                className={`block rounded-xl py-3 pl-4 ${
+                  activeNav === "about" ? "active" : ""
+                }`}
+              >
+                <FontAwesomeIcon icon="book" className="pr-2" />
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
